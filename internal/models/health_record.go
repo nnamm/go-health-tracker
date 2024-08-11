9fba36526e1dece14011fe1df540362810dc9f42
Takashi Hanamura <t.nnamm@gmail.com>
2024-08-11 17:38:07 +0900

    add custom marshal/unmarshal funcs for Date (#1)

--- internal/models/health_record.go
+++ internal/models/health_record.go
@@ -1,12 +1,41 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type HealthRecord struct {
-	ID int64 `json:"id"`
-	//Date      string    `json:"date"`
+	ID        int64     `json:"id"`
 	Date      time.Time `json:"date"`
 	StepCount int       `json:"step_count"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (hr *HealthRecord) MarshalJSON() ([]byte, error) {
+	type Alias HealthRecord
+	return json.Marshal(&struct {
+		Date string `json:"date"`
+		*Alias
+	}{
+		Date:  hr.Date.Format("2006-01-02"),
+		Alias: (*Alias)(hr),
+	})
+}
+
+func (hr *HealthRecord) UnmarshalJSON(date []byte) error {
+	type Alias HealthRecord
+	aux := &struct {
+		Date string `json:"date"`
+		*Alias
+	}{
+		Alias: (*Alias)(hr),
+	}
+	if err := json.Unmarshal(date, &aux); err != nil {
+		return err
+	}
+	var err error
+	hr.Date, err = time.Parse("2006-01-02", aux.Date)
+	return err
 }
