14ab0a70c8f59bbda6917f0608fbce3b3c19e34e
nnamm <t.nnamm@gmail.com>
2025-04-01 22:25:12 +0900

    improvement helpers completed

--- internal/handlers/health_records_test.go
+++ internal/handlers/health_records_test.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/nnamm/go-health-tracker/internal/database/mock"
-	"github.com/nnamm/go-health-tracker/internal/testutil"
+	testutil "github.com/nnamm/go-health-tracker/internal/handlertest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
